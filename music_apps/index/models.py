de5f5a17836c261506bc831c870a9c0b0e7668e5
JasonmerND <707901301@qq.com>
2019-07-22 08:31:39 +0800

    Project directory created successfully

---
+++ music_apps/index/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+
+# Create your models here.
+
+class Laber(models.Model):
+    pass
+
+class Song(models.Model):
+    pass
+
+class Dynamic(models.Model):
+    pass
+
+class Comment(models.Model):
+    pass
