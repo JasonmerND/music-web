7fc92cb981d1160e03a0ceb9e9ab0231393b2914
JasonmerND <707901301@qq.com>
2019-07-22 15:30:36 +0800

    '歌曲排行榜页面完成'

--- music_apps/ranking/urls.py
+++ music_apps/ranking/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('ranking', views.rankingView, name='ranking'),
+    path('', views.rankingView, name='ranking'),
 ]
--- music_apps/ranking/views.py
+++ music_apps/ranking/views.py
@@ -1,6 +1,20 @@
 from django.shortcuts import render
-
+from index.models import *
 # Create your views here.
 
 def rankingView(request):
-    pass
+    
+    # 热门搜索
+    search_song = Dynamic.objects.select_related('song').order_by('-dynamic_search').all()[:4]
+
+    # 流派
+    all_list = Song.objects.values('song_type').distinct()
+
+    # 歌曲列表信息
+    song_type = request.GET.get('type','')
+    if song_type:
+        song_info = Dynamic.objects.select_related('song').filter(song__song_type=song_type).order_by('-dynamic_plays').all()[:10]
+    else:
+        song_info = Dynamic.objects.select_related('song').order_by('-dynamic_plays').all()[:10]
+    
+    return render(request, 'ranking.html', context=locals())
